5087482e1bf86aacaea3f360e521fb856f8b361f
github username <github email address>
2018-11-25 22:06:43 -0500

    third commit

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,14 +1,16 @@
-import bigPhoto from '../assets/bigPhoto';
+// import bigPhoto from '../assets/bigPhoto';
 import smallPhoto from '../assets/smallPhoto';
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = smallPhoto;
+export default () => {
+  const image = document.createElement('img');
+  image.src = smallPhoto;
 
-document.body.appendChild(image);
+  document.body.appendChild(image);
+};
 
 
-const bigImage = document.createElement('img');
-bigImage.src = bigPhoto;
-
-document.body.appendChild(bigImage);
+// const bigImage = document.createElement('img');
+// bigImage.src = bigPhoto;
+//
+// document.body.appendChild(bigImage);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import sum from './sum';
-import './image_viewer';
+// import sum from './sum';
+// import './image_viewer';
+//
+// const total = sum(10, 5);
+// console.log(total);
+const button = document.createElement('button');
+button.innerText = 'Click me';
 
-const total = sum(10, 5);
-console.log(total);
+button.onclick = () => {
+  System.import('./image_viewer').then(module => {
+    module.default();
+  });
+};
+
+
+document.body.appendChild(button);
